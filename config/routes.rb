c79c9204fff8fe903e7ed928e01a668e201d5667
tanabetetsuya <tetsumagic123@gmail.com>
2021-07-01 12:02:23 +0000

    [Update] ルーティングの追加・修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,47 +1,26 @@
 Rails.application.routes.draw do
+  devise_for :admins
   namespace :admin do
     get 'inquiries/index'
+     get 'homes/top'
   end
-  namespace :admin do
-    get 'homes/top'
-  end
-  get 'events/new'
-  get 'events/create'
-  get 'events/edit'
-  get 'events/update'
-  get 'events/show'
-  get 'events/index'
-  get 'events/destroy'
-  get 'comments/create'
-  get 'comments/destroy'
-  get 'likes/new'
-  get 'likes/create'
-  get 'inquiries/new'
-  get 'inquiries/create'
-  get 'today_exercises/new'
-  get 'today_exercises/create'
-  get 'today_exercises/edit'
-  get 'today_exercises/update'
-  get 'today_exercises/destroy'
-  get 'exercises/new'
-  get 'exercises/create'
-  get 'exercises/show'
-  get 'exercise_suggestions/index'
-  get 'exercise_suggestions/create'
-  get 'exercise_suggestion/index'
-  get 'exercise_suggestion/create'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  get 'show/edit'
-  get 'show/update'
-  get 'exercise_menus/index'
-  get 'exercise_menus/new'
-  get 'exercise_menus/create'
-  get 'exercise_menus/show'
-  get 'homes/top'
-  get 'homes/about'
-  devise_for :admins
+
   devise_for :users
+  resources :exercise_menus, only: [:new, :create, :show, :index]
+  get '/exercise_menus/:id/registration' => 'exercise_menus#registration'
+  resources :users, only: [:show, :edit, :update] do
+    resource :likes, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
+  end
+  get '/users/check' => 'users#check'
+  put '/users' => 'users#resign'
+  resources :exercise_suggestions, only: [:index, :create]
+  resources :exercises, only: [:new, :create, :show]
+  resources :today_exercises, only: [:new, :create, :edit, :update, :destroy]
+  resources :inquiries, only: [:new, :create]
+  post '/inquiries/:id/confirm' => 'inquiries#confirm'
+  get '/inquiries/complete' => 'inquiries#complete'
+  resources :events
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

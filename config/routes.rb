c7ff01b9c37d84aac146176fc783700e55ee4fac
tanabetetsuya <tetsumagic123@gmail.com>
2021-07-07 07:49:39 +0000

    [Update] ルーティング一部修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,10 @@ Rails.application.routes.draw do
      get 'homes/top'
   end
 
-  devise_for :users, :controllers => {
-    :registrations =>'users/registrations',
-    :sessions => 'users/sessions'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
-  devise_scope :user do
-    get "sign_in", :to => "users/sessions#new"
-    get "sign_out", :to => "users/sessions#destroy"
-  end
   root to: 'homes#top'
   resources :exercise_menus, only: [:new, :create, :show, :index]
   get '/exercise_menus/:id/registration' => 'exercise_menus#registration'

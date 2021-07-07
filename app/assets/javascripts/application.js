0a9945dce5b9e592903d42715f639176276d5d29
tanabetetsuya <tetsumagic123@gmail.com>
2021-07-07 12:07:09 +0000

    [Add] 評価機能の作成

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,22 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require popper
+//= bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery.raty.js
 //= require_tree .
+/*!
+ * Raty - A Star Rating Plugin
+ *
+ * The MIT License
+ *
+ * author:  Washington Botelho
+ * github:  wbotelhos/raty
+ * version: 3.0.0
+ *
+ */
+
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
+
+  validates :rate, numericality: {
+      less_than_or_equal_to: 5,
+      greater_than_or_equal_to: 1
+  }, presence: true
 end

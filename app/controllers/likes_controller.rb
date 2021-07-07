71d5c7104c1b51b1021f39f83d4fe50e64b5b11b
tanabetetsuya <tetsumagic123@gmail.com>
2021-07-07 10:07:52 +0000

    [Add] いいね機能の実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,13 @@
 class LikesController < ApplicationController
-  def new
+  def destroy
+    like = current_user.likes.find_by(user_id: user.id)
+    like.destroy
+    redirect_to users_path
   end
 
   def create
+    like = current_user.likes.new
+    like.save
+    redirect_to users_path
   end
 end

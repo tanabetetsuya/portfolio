2c962b44dc5f2b86611a0345b33947ebcc11448f
tanabetetsuya <tetsumagic123@gmail.com>
2021-07-18 03:42:39 +0000

    [Update] テストコードの一部修正

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,10 +13,15 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'capybara/rspec'
 RSpec.configure do |config|
+  config.include Capybara::DSL
+  config.before(:each, type: :system) do
+   driven_by :rack_test
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+  end
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods

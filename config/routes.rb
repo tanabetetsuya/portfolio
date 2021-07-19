0dda2bdb432a0513a38eaa390d14bcc1213ce717
tanabetetsuya <tetsumagic123@gmail.com>
2021-07-19 13:40:55 +0000

    [Update] ルーティング一部修正

--- config/routes.rb
+++ config/routes.rb
@@ -15,18 +15,18 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
     resource :likes, only: [:create, :destroy]
   end
-  get '/exercise_menus/:id/registration' => 'exercise_menus#registration'
+  post '/exercise_menus/:id/suggestion' => 'exercise_menus#suggestion'
   resources :users, only: [:show, :index, :edit, :update] 
   get '/users/check' => 'users#check'
   put '/users' => 'users#resign'
-  resources :exercise_suggestions, only: [:index, :create]
+  resources :exercise_suggestions, only: [:new, :create, :index]
   resources :exercises, only: [:new, :create, :show]
   resources :today_exercises, only: [:new, :create, :edit, :update, :destroy]
   resources :inquiries, only: [:new, :create]
   post '/inquiries/:id/confirm' => 'inquiries#confirm'
   get '/inquiries/complete' => 'inquiries#complete'
   resources :events
-  get '/youtubes' => 'youtubes#index'
+  resources :youtubes, only: [:new, :create, :index]
   get '/youtubes/search' => 'youtubes#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

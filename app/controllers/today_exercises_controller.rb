563ef309023171fa2a8a5a60bbad977f56cf56c4
tanabetetsuya <tetsumagic123@gmail.com>
2021-07-21 10:23:39 +0000

    [Update] createアクションの修正

--- app/controllers/today_exercises_controller.rb
+++ app/controllers/today_exercises_controller.rb
@@ -3,6 +3,16 @@ class TodayExercisesController < ApplicationController
   end
 
   def create
+      #youtube = Youtube.find(params[:youtube_id])
+      #today_exercise = TodayExercise.new(today_exercise_params)
+      #today_exercise.exercise_menu_id = exercise_menu.id
+      #today_exercise.youtube_id = youtube.id
+      #@today_exercise.title = snippet.title
+      #@today_exercise.published_at = snippet.published_at
+      #@today_exercise.channel_title = snippet.channel_title
+      #@today_exercise.video_id = item.id.video.id
+      #today_exercise.save
+      #redirect_to new_exercise_path
   end
 
   def edit
@@ -12,5 +22,10 @@ class TodayExercisesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def today_exercise_params
+    params.require(:today_exercise).permit(:youtube_id, :exercise_menu_id, :title, :channel_title, :published_at, :video_id)
   end
 end

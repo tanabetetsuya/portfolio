03c22819224a54ddc60bedb7c3337b045d0565a5
tanabetetsuya <tetsumagic123@gmail.com>
2021-08-01 11:18:51 +0000

    [Update] バリデーションの追加

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,8 +1,8 @@
 class Inquiry include ActiveModel::Model #DBをしようしないためActiveModelを使用
-  #belongs_to :user
-  
+
   attr_accessor :name, :reply_email, :message #読み取りも書き取りもできるオブジェクトの属性を定義
-  
+
   validates :name, :presence => {:message => '名前を入力してください'}
   validates :reply_email, :presence => {:message => 'メールアドレスを入力してください'}
+  validates :message, :presence => {:message => 'メッセージ内容を入力してください'}
 end

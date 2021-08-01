541b56ed8e0a7c3b38c0540768046a38431d3395
tanabetetsuya <tetsumagic123@gmail.com>
2021-08-01 11:16:51 +0000

    [Update] 送信先メールアドレスの修正

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,8 +1,9 @@
 class InquiryMailer < ApplicationMailer
   default from: ENV['SEND_MAIL'] #送信元のメールアドレス
+  default to: ENV['SEND_MAIL']#送信先のメールアドレス
 
   def received_email(inquiry)
     @inquiry = inquiry
-    mail(:to => inquiry.reply_email, :subject => 'お問い合わせを承りました')
+    mail(:subject => 'お問い合わせを承りました')
   end
 end

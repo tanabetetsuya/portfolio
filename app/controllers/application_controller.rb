023123d100f7799945fd71da8ddd1530ef508f3f
tanabetetsuya <tetsumagic123@gmail.com>
2021-08-01 11:08:45 +0000

    [Add] ストロングパラメーターの項目追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_up_path_for(resource)
-    root_path
-  end
+  protected
 
-  def after_sign_in_path_for(resource)
-    root_path
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile_image, :provider, :uid])
   end
 end

94d315e0be31637bd798d5a39e83729a488de312
tanabetetsuya <tetsumagic123@gmail.com>
2021-08-08 22:36:22 +0000

    [Add] secret_keyの追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '09e605e8ca58a5e7b25af2f16facb3c3a7a0d6d8e6acb1c0a493000dd26030de4076460852a2cfe3e174781addd0e416d84c54d758f5a2a8227c9318a503d46d'

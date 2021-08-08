0ec6874b4a57ac9dd5a9408264c04434c053a289
tanabetetsuya <tetsumagic123@gmail.com>
2021-08-08 13:06:59 +0000

    [Update] def inquiry_backの削除

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,9 +1,12 @@
 class InquiriesController < ApplicationController
+  before_action :authenticate_user!
+  #before_action :inquiry_back, only: [:confirmation, :complete]
+
   def index
     @inquiry = Inquiry.new
     render :action => 'index'
   end
-  
+
   def confirmation
     @inquiry = Inquiry.new(inquiry_params)
     if @inquiry.valid?
@@ -12,17 +15,21 @@ class InquiriesController < ApplicationController
       render :action => 'index'
     end
   end
-  
+
   def complete
     @inquiry = Inquiry.new(inquiry_params)
     InquiryMailer.received_email(@inquiry).deliver
     render :action => 'complete'
   end
-  
-  def create
-  end
-  
-  private 
+
+  #def inquiry_back
+    #@inquiry = Inquiry.new(inquiry_params)
+    #if @inquiry.blank?
+      #redirect_to inquiries_path
+    #end
+  #end
+
+  private
   def inquiry_params
     params.require(:inquiry).permit(:name, :reply_email, :message)
   end

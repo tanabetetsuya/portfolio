9fb73872129d1406ff865f906e666c50cb7b3b3c
tanabetetsuya <tetsumagic123@gmail.com>
2021-08-09 22:48:26 +0000

    [Add]　退会機能追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update]
+
+  def show
+    @user = current_user
+    @exercise_menus = current_user.exercise_menus.all.page(params[:page]).per(5)
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  def check
+    @user = current_user
+  end
+
+  def resign
+    @user =current_user
+    @user.update(is_valid: "退会")
+    reset_session
+    redirect_to new_user_session_path
+  end
+
+  def correct_user #他のユーザーの情報を編集できないように設定
+    user = User.find(params[:id])
+    if current_user != user
+      redirect_to exercise_menus_path
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :profile_image, :is_valid)
+  end
+end

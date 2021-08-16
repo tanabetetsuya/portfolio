e00a7a5b24ebaef376fb9f58ea128f902eb85621
tanabetetsuya <tetsumagic123@gmail.com>
2021-08-16 12:05:39 +0000

    [Add] config.action_mailer.default_url_optionsの追記

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
     enable_starttls_auto:  true
 
   }
+
+  config.action_mailer.default_url_options = { protocol: 'https',host:'exercise-suggestion.work'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.

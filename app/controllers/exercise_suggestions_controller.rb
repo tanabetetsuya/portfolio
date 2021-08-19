c595556a40524793c660f6f59428022fc219af99
tanabetetsuya <tetsumagic123@gmail.com>
2021-08-19 09:46:36 +0000

    [Update] exercise_video_params => exercise_suggestion_params

--- app/controllers/exercise_suggestions_controller.rb
+++ app/controllers/exercise_suggestions_controller.rb
@@ -8,7 +8,7 @@ class ExerciseSuggestionsController < ApplicationController
 
   def create
     #binding.irb
-    exercise_suggestion = ExerciseSuggestion.new(exercise_video_params)
+    exercise_suggestion = ExerciseSuggestion.new(exercise_suggestion_params)
     exercise_suggestion.save
     redirect_to exercise_suggestions_path
   end

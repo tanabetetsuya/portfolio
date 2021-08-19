7f95c2865e5c81e02c8908da29b3398d32c8f5fc
tanabetetsuya <tetsumagic123@gmail.com>
2021-08-19 09:34:09 +0000

    [Add] マイグレーションファイルの作成

---
+++ db/migrate/20210819091309_add_columns_to_exercise_suggestions.rb
@@ -0,0 +1,7 @@
+class AddColumnsToExerciseSuggestions < ActiveRecord::Migration[5.2]
+  def change
+    add_column :exercise_suggestions, :title, :string
+    add_column :exercise_suggestions, :published_at, :datetime
+    add_column :exercise_suggestions, :channel_title, :string
+  end
+end
